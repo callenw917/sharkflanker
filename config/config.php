0602af14c8906fbfa08b06bb19118e9ddacca03c
Callen Wessels <callenrwessels@gmail.com>
2020-08-03 12:00:07 -0500

    DB bug

--- config/config.php
+++ config/config.php
@@ -10,16 +10,14 @@ $timezone = date_default_timezone_set("America/Chicago");
 //$connection = mysqli_connect("localhost","root","","flanker");
 
 // Heroku ClearDB Connection
- $db = parse_url(getenv("DATABASE_URL"));
+ $url=parse_url(getenv("DATABASE_URL"));
 
-$pdo = new PDO("pgsql:" . sprintf(
-    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
-    $db["host"],
-    $db["port"],
-    $db["user"],
-    $db["pass"],
-    ltrim($db["path"], "/")
-));
+  $server   = $url["host"];
+  $username = $url["user"];
+  $password = $url["pass"];
+  $db       = substr($url["path"],1);
+  $host     = "mysql:host=$server;dbname=$db";
+  $connection = mysqli_connect($server , $username, $password, $db);
 
 
 if(mysqli_connect_errno()) {
