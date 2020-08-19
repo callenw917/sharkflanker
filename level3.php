f8bbc34dc5c0ee104748e623a476eaef4c8cf8c3
alueckenhoff-uta <67869095+alueckenhoff-uta@users.noreply.github.com>
2020-08-19 11:22:47 -0500

    Level3 added
    *js file
    *php file
    *working some styling changes need to be made

---
+++ level3.php
@@ -0,0 +1,30 @@
+<!--level 3: grid pattern with different sizes of sharks-->
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="CSS/style.css">
+    <script src="JS/level3.js"></script>
+</head>
+
+<body>
+    <h1 id="Title">Level 3</h1>
+    <h3>Which way is the special shark facing?</h3>
+            <img src="Images/shark.svg" class="shark row1 col1 absolute" style="height= auto" id="shark00">
+            <img src="Images/shark.svg" class="shark row1 col2 absolute" style="height= auto" id="shark01">
+            <img src="Images/shark.svg" class="shark row1 col3 absolute" style="height= auto" id="shark02">
+
+            <img src="Images/shark.svg" class="shark row2 col1 absolute" style="height= auto" id="shark10">
+            <img src="Images/shark.svg" class="shark row2 col2 absolute" style="height= auto" id="shark11">
+            <img src="Images/shark.svg" class="shark row2 col3 absolute" style="height= auto" id="shark12">
+
+            <img src="Images/shark.svg" class="shark row3 col1 absolute" style="height= auto" id="shark20">
+            <img src="Images/shark.svg" class="shark row3 col2 absolute" style="height= auto" id="shark21">
+            <img src="Images/shark.svg" class="shark row3 col3 absolute" style="height= auto" id="shark22">
+    <!--Inserting the button panel template-->
+    <?php include 'panel_template.php'; ?>
+
+</body>
+</html>
